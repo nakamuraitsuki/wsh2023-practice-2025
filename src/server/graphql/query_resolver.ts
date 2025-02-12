9160ecdc61796459ee247e84725aed4d42c4c5b1
Itsuki Nakamura <132897891+nakamuraitsuki@users.noreply.github.com>
2025-02-13 02:03:43 +0900

    Revert "revert:おそらく単一なら普通に取得した方が早い"

--- src/server/graphql/query_resolver.ts
+++ src/server/graphql/query_resolver.ts
@@ -42,8 +42,16 @@ export const queryResolver: QueryResolver = {
       where: { id: args.id },
     });
   },
-  recommendations: () => {
-    return dataSource.manager.find(Recommendation);
+  recommendations: async () => {
+    const res = await dataSource.manager
+      .createQueryBuilder(Recommendation, 'recommendation')
+      .leftJoinAndSelect('recommendation.product', 'product')
+      .leftJoinAndSelect('product.media', 'media')
+      .leftJoinAndSelect('product.offers', 'offers')
+      .leftJoinAndSelect('media.file', 'file')
+      .getMany();  // Recommendationをその関連項目（Product, Media, Offersなど）と一緒に取得
+    console.log("res", res);
+    return res;
   },
   user: (_parent, args) => {
     return dataSource.manager.findOneOrFail(User, {
