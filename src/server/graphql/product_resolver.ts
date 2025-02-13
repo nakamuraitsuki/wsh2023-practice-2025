2862f94073a20a0649dd4ca5bd4e8cca8f58ce40
nakamuraitsuki <isukin@gmail.com>
2025-02-13 17:02:46 +0900

    feat/add time log

--- src/server/graphql/product_resolver.ts
+++ src/server/graphql/product_resolver.ts
@@ -8,28 +8,42 @@ import { GraphQLModelResolver } from './model_resolver';
 
 export const productResolver: GraphQLModelResolver<Product> = {
   media: async (parent) => {
+    console.time('productResolver.media');
+
     if (parent.media != null) {
+      console.timeEnd('productResolver.media');
       return parent.media;
     }
-    return await dataSource.manager.find(ProductMedia, {
+    const res = await dataSource.manager.find(ProductMedia, {
       where: { product: { id: parent.id } },
       relations: { file: true },
     });
+
+    console.timeEnd('productResolver.media');
+    return res;
   },
   offers: async (parent) => {
+    console.time('productResolver.offers');
     if (parent.offers != null) {
+      console.timeEnd('productResolver.offers');
       return parent.offers;
     }
-    return await dataSource.manager.find(LimitedTimeOffer, {
+    const res = await dataSource.manager.find(LimitedTimeOffer, {
       where: { product: { id: parent.id } },
     });
+    console.timeEnd('productResolver.offers');
+    return res;
   },
   reviews: async (parent) => {
+    console.time('productResolver.reviews');
     if (parent.reviews != null) {
+      console.timeEnd('productResolver.reviews');
       return parent.reviews;
     }
-    return await dataSource.manager.find(Review, {
+    const res = await dataSource.manager.find(Review, {
       where: { product: { id: parent.id } },
     });
+    console.timeEnd('productResolver.reviews');
+    return res;
   },
 };
