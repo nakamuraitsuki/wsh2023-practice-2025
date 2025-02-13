ebb9dc4ead24e85be915125058e376aecd5c726f
nakamuraitsuki <isukin@gmail.com>
2025-02-13 20:46:11 +0900

    feat: JOIN自体に相当な時間がかかっていると予想。データローダーの方針に戻した

--- src/server/graphql/feature_item_resolver.ts
+++ src/server/graphql/feature_item_resolver.ts
@@ -1,9 +1,21 @@
 import type { FeatureItem } from '../../model/feature_item';
+import { Product } from '../../model/product';
 
 import type { GraphQLModelResolver } from './model_resolver';
+import { dataSource } from '../data_source';
+import DataLoader from 'dataloader';
 
 export const featureItemResolver: GraphQLModelResolver<FeatureItem> = {
-  product: (parent) => {
-    return parent.product;
+  product: async(parent) => {
+    return await ProductLoader.load(parent.id);
   },
 };
+
+const ProductLoader = new DataLoader(async (ids: readonly number[]) => {
+  const products = await dataSource
+    .createQueryBuilder(Product, 'product')
+    .whereInIds(ids)
+    .select(['product.id', 'product.name', 'product.price', 'product.description'])
+    .getMany();
+  return ids.map((id) => products.find((product) => product.id === id)) as Product[];
+});
