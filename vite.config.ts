da97ed1f47d658e1c55e4b11c304e47828330f58
nakamuraitsuki <isukin@gmail.com>
2025-02-18 01:54:58 +0900

    feat: buildできるようにコンプレッサー無くした

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 import { visualizer } from 'rollup-plugin-visualizer';
 import viteImagemin from 'vite-plugin-imagemin';
-import viteCompress from 'vite-plugin-compress';
 
 import { getFileList } from './tools/get_file_list';
 
@@ -56,14 +55,14 @@ export default defineConfig(async () => {
           optimizationLevel: 3,
         },
         mozjpeg: {
-          quality: 80,
+          quality: 70,
         },
         optipng: {
-          optimizationLevel: 5,
+          optimizationLevel: 3,
         },
         pngquant: {
           quality: [0.65, 0.8],
-          speed: 4,
+          speed: 10,
         },
         svgo: {
           plugins: [
@@ -72,12 +71,8 @@ export default defineConfig(async () => {
           ],
         },
         webp: {
-          quality: 80,
+          quality: 70,
         }
-      }),
-      viteCompress({  // 追加した圧縮プラグイン
-        verbose: true, // 圧縮の詳細情報を表示する
-        threshold: 10240, // 10KB以上のファイルを圧縮
       }),
     ],
   };
