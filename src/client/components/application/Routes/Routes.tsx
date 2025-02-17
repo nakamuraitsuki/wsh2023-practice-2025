7097c3a3329a3307313240f16ebb1191987313d0
Itsuki Nakamura <132897891+nakamuraitsuki@users.noreply.github.com>
2025-02-17 22:43:56 +0900

    Revert "feat: add page lazy"

--- src/client/components/application/Routes/Routes.tsx
+++ src/client/components/application/Routes/Routes.tsx
@@ -1,13 +1,11 @@
-import { FC, Suspense } from 'react';
+import type { FC } from 'react';
 import * as Router from 'react-router-dom';
-import React from 'react';
 
-// React.lazyを使用してコンポーネントを遅延ロード
-const NotFound = React.lazy(() => import('../../../pages/NotFound').then(module => ({ default: module.NotFound })));
-const Order = React.lazy(() => import('../../../pages/Order').then(module => ({ default: module.Order })));
-const OrderComplete = React.lazy(() => import('../../../pages/OrderComplete').then(module => ({ default: module.OrderComplete })));
-const ProductDetail = React.lazy(() => import('../../../pages/ProductDetail').then(module => ({ default: module.ProductDetail })));
-const Top = React.lazy(() => import('../../../pages/Top').then(module => ({ default: module.Top })));
+import { NotFound } from '../../../pages/NotFound';
+import { Order } from '../../../pages/Order';
+import { OrderComplete } from '../../../pages/OrderComplete';
+import { ProductDetail } from '../../../pages/ProductDetail';
+import { Top } from '../../../pages/Top';
 
 import { useScrollToTop } from './hooks';
 
@@ -15,14 +13,12 @@ export const Routes: FC = () => {
   useScrollToTop();
 
   return (
-    <Suspense fallback={null}>
-      <Router.Routes>
-        <Router.Route element={<Top />} path="/" />
-        <Router.Route element={<ProductDetail />} path="/product/:productId" />
-        <Router.Route element={<Order />} path="/order" />
-        <Router.Route element={<OrderComplete />} path="/order/complete" />
-        <Router.Route element={<NotFound />} path="*" />
-      </Router.Routes>
-    </Suspense>
+    <Router.Routes>
+      <Router.Route element={<Top />} path="/" />
+      <Router.Route element={<ProductDetail />} path="/product/:productId" />
+      <Router.Route element={<Order />} path="/order" />
+      <Router.Route element={<OrderComplete />} path="/order/complete" />
+      <Router.Route element={<NotFound />} path="*" />
+    </Router.Routes>
   );
 };
