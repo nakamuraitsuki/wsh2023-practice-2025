91f1de4fe452a7a80203f3e1ef82840029a24b1d
nakamuraitsuki <isukin@gmail.com>
2025-02-21 00:43:23 +0900

    feat: 表示おかしいのでN+1発生させた

--- src/server/graphql/feature_item_resolver.ts
+++ src/server/graphql/feature_item_resolver.ts
@@ -1,21 +1,9 @@
 import type { FeatureItem } from '../../model/feature_item';
-import { Product } from '../../model/product';
 
 import type { GraphQLModelResolver } from './model_resolver';
-import { dataSource } from '../data_source';
-import DataLoader from 'dataloader';
 
 export const featureItemResolver: GraphQLModelResolver<FeatureItem> = {
-  product: async(parent) => {
-    return await ProductLoader.load(parent.id);
+  product: (parent) => {
+    return parent.product;
   },
 };
-
-const ProductLoader = new DataLoader(async (ids: readonly number[]) => {
-  const products = await dataSource
-    .createQueryBuilder(Product, 'product')
-    .whereInIds(ids)
-    .select(['product.id', 'product.name', 'product.price', 'product.description'])
-    .getMany();
-  return ids.map((id) => products.find((product) => product.id === id)) as Product[];
-});
