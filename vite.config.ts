aa28e4269b053f727fafb78386a755fb5a9451fa
nakamuraitsuki <isukin@gmail.com>
2025-02-27 16:13:17 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig(async () => {
             if (id.includes('@apollo/client')) {
               return 'apollo';
             }
-            if (id.includes('react')) {
+            if (id.includes('react') || id.includes('formik')) {
               return 'react';
             }
             if (id.includes('node_modules')) {
