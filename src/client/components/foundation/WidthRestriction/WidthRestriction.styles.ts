90af68c15ece47c92a53fda6af63beb0b8945404
nakamuraitsuki <isukin@gmail.com>
2025-02-27 20:51:38 +0900

    fix: 明らかなずれを見つけたので改善

--- src/client/components/foundation/WidthRestriction/WidthRestriction.styles.ts
+++ src/client/components/foundation/WidthRestriction/WidthRestriction.styles.ts
@@ -2,7 +2,6 @@ import { css } from '@emotion/css';
 
 export const container = () => css`
   width: 100%;
-  aspect-ratio: 16 / 9;
 `;
 
 export const inner = ({ width }: { width: number | undefined }) => css`
